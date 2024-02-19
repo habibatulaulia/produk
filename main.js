9a85e0a92cd0328a4c8a0dd5f6bf8f867b3a4f8d
habibatulaulia <habibatulaulia76@gmail.com>
2024-02-19 11:23:42 +0700

    menampilkan daftar produk

---
+++ main.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9uJSx87wLWMehUFmpdboN9sBM9s_tN1M",
+  authDomain: "insan-cemerlang-41b6b.firebaseapp.com",
+  projectId: "insan-cemerlang-41b6b",
+  storageBucket: "insan-cemerlang-41b6b.appspot.com",
+  messagingSenderId: "1057804782651",
+  appId: "1:1057804782651:web:bf911a5c4bff4f7bef201e"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarProduk() {
+  const refDokumen = collection(db, "produk");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+  
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id, 
+      nama: dok.data().nana,
+      harga: dok.data().harga,
+      stok: dok.data().stok,
+    });
+  });
+  
+  return hasil;
+}
